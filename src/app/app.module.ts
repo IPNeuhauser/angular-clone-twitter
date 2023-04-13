0908b332180ab99b1cb901d1011d56511c385b24
IPNeuhauser <ipneuhauser@gmail.com>
2023-04-12 22:18:26 -0300

    Desenvolvimento dos componentes header footer e new-post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NewPostComponent,
+    TweetComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
